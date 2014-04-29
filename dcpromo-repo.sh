f88a2f6c1734e1ddb76e3fde0150265c42096cc6
Niklas Andersson <niklas.andersson@openforce.se>
2014-04-29 20:09:30 +0200

    Fixed dpkg-reconfigure problem

--- dcpromo-repo.sh
+++ dcpromo-repo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
 add-apt-repository ppa:niklas-andersson/dcpromo
 apt-get update
 apt-get install dcpromo
