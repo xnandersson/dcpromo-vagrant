616559e41001a2d2f6426acbb4fa8eee8be97585
Niklas Andersson <niklas.andersson@openforce.se>
2014-05-01 14:54:31 +0200

    Cleaned up dcpromo-rep.sh

--- dcpromo-repo.sh
+++ dcpromo-repo.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
-dpkg-reconfigure locales
-
 add-apt-repository ppa:niklas-andersson/dcpromo
 apt-get update
 debconf-set-selections /vagrant/dcpromo.debconf
